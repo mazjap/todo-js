0f31b3ecc3d31f41e22e4d1587d0d59bea853404
Jordan Christensen <jordan.c4922@gmail.com>
2021-03-07 11:31:16 -0700

    Code cleanup, fix issues with todo item names not displaying, change login text to username when logged in, etc.

--- js/classes.js
+++ js/classes.js
@@ -4,9 +4,9 @@ class Task {
     constructor(title, dueDate, description = "No description", isCompleted = false, id = generateUUID()) {
         this.title = title;
         this.description = description;
-        this.dueDate = dueDate;
         this.isCompleted = isCompleted;
         this.id = id;
+        this.due = dueDate
     }
 
     setTitle(newTitle) {
@@ -17,25 +17,23 @@ class Task {
         this.description = newDesc;
     }
 
-    setDueDate(newDate) {
-        this.dueDate = newDate;
-    }
-
     toggleCompleted() {
         this.isCompleted = !this.isCompleted;
     }
 
-    formattedDate() {
-        return this.dueDate.getFullYear() + "/" +
-               this.dueDate.getMonth() + "/" +
-               this.dueDate.getDate() + " " +
-               this.dueDate.getHours() + ":"  +
-               this.dueDate.getMinutes(); // Output: `2021/02/23 9:49`
+    dateToString() {
+        let dateString = this.dueDate.getFullYear() + "/" +
+                         this.dueDate.getMonth() + "/" +
+                         this.dueDate.getDate() + " " +
+                         this.dueDate.getHours() + ":"  +
+                         this.dueDate.getMinutes(); // Output: `2021/02/23 9:49`
+
+        return dateString;
     }
 }
 
 class List {
-    constructor(name, tasks, id = generateUUID()) {
+    constructor(name, tasks = [], id = generateUUID()) {
         this.name = name
         this.tasks = tasks;
         this.id = id;
@@ -46,15 +44,35 @@ class List {
     }
 
     removeTask(id) {
-        for (let i=0; i<this.tasks; i++) {
-            if (this.tasks[i].id == id) {
+        let ret;
+
+        for (let i=0; i<this.tasks.length; i++) {
+            const task = this.tasks[i];
+            console.log(task.id);
+            console.log(id);
+            if (task.id == id) {
+                ret = task;
                 this.tasks.splice(i, 1);
             }
         }
+
+        return ret;
+    }
+
+    hasCompleted() {
+        console.log(this.tasks)
+        for (let todo of this.tasks) {
+            console.log(todo);
+            if (todo.isCompleted) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     clearCompletedTodos() {
-        for (todo of this.tasks) {
+        for (let todo of this.tasks) {
             if (todo.isCompleted) {
                 this.removeTask(todo.id);
             }
@@ -65,84 +83,98 @@ class List {
 class User {
     constructor(username, password) {
         this.username = username;
-        this.id = hash(username, password).toString();
+        this.id = this.hash(username, password);
         this.lists = this.fetchLists();
         console.log("Logged in with hash: " + this.id);
     }
 
     removeList(id) {
-        for (let i=0; i<this.lists; i++) {
+        let ret;
+        for (let i=0; i<this.lists.length; i++) {
             if (this.lists[i].id == id) {
+                ret = this.lists[i];
                 this.lists.splice(i, 1);
+                User.saveChanges();
             }
         }
+
+        return ret;
     }
 
     addList(list) {
-        this.lists.push(list)
+        this.lists.push(list);
+        User.saveChanges();
     }
-
 
     fetchLists() {
-        let val = store.getItem(this.id)
-        console.log("Val: " + val);
-        return JSON.parse(val ?? '[]');
+        return JSON.parse(store.getItem(this.id) ?? '[]');
     }
 
-    static current = null;
+    hash(username, password) {
+        // Imported hashing algo
+        return hex_sha512(username + password)
+    }
+
+    static current = null; //new User("jordan", "password");
 
     static saveChanges() {
-        let stringcheese = JSON.stringify(this.lists);
-        console.log(stringcheese);
-        store.setItem(this.id, stringcheese);
+        store.setItem(User.current.id, JSON.stringify(User.current.lists));
     }
 }
 
 class HTMLGenerator {
-    static generateListItem(item) {
+    static generateListItem(item, listId) {
         return `
-        <div id="list-item">
+        <div class="list-item">
             <div class="checkbox-container">
-                <input type="checkbox" class="is-complete">
+                <input type="checkbox" class="is-complete" id="${item.id}-checkbox" onchange="toggleItem('${item.id}', '${listId}')" ${item.isCompleted ? "checked" : ""}>
                 <div class="text">
-                    <h3>${item.name}</h3>
+                    <h3>${item.title}</h3>
                     <p>${item.description}</p>
                 </div>
             </div>
             <div class="actions-container">
-                <span class="dueDate">${item.formattedDate}</span>
-                <img src="" alt="">
+                <span class="dueDate">${item.due}</span>
                 <input type="checkbox" class="is-expanded">
-                <button onclick="">-</button>
+                <button class="delete-button" onclick="deleteTask('${item.id}', '${listId}')">-</button>
             </div>
         </div>`;
     }
 
     static generateList(list) {
-        let str = '<div id="list">'
-        for (let item of list?.tasks ?? []) {
-            str += this.generateListItem(item)
+        let str = `<div id="list"><p id="new-task" onclick="displayModal(this.id, '${list.id}')">+</p>`;
+
+        if (list.tasks.length !== 0) {
+            for (let item of list.tasks) {
+                str += this.generateListItem(item, list.id);
+            }
+
+            str += `<button onclick="clearCompleted('${list.id}')">Clear Completed</button>`;
+        } else {
+            str += "<p class='error-message'>No tasks. Press the \`+\` to make a new one.</p>";
         }
 
-        return str + "</div>"
+        return str + "</div>";
     }
 
     static generateListPreview(list) {
         return `
-        <div class="list-preview">
-            <h3 class="text">${list.name}</h3>
+        <div class="list-preview" onclick="listState('${list?.id ?? 0}')">
+            <h3 class="text">${list?.name ?? "No Name"}</h3>
 
             <div class="count">
                 <p>Count:</p>
                 <p class="todo-count">${list?.tasks?.length ?? 0}</p>
             </div>
+
+            <button class="delete-button" onclick="deleteList('${list.id}')">-</button>
         </div>`;
     }
 
     static generateMyLists() {
         const user = User.current
 
-        let html =  user ? `<p id="new-list" onclick="displayModal('new-list')">+</p>` : '';
+        let html =  user ? `<p id="new-list" onclick="displayModal(this.id)">+</p>` : '';
 
         if ((user?.lists ?? []).length !== 0) {
             for (let list of user?.lists ?? []) {
@@ -202,12 +234,22 @@ class HTMLGenerator {
         </div>`;
     }
 
-    static generateModal(state) {
+    static generateModal(state, id) {
         switch (state) {
             case "new-list":
-                return `<input id="new-list-title" type="text" placeholder="List Title">'
+                return `
+                <input id="new-list-title" type="text" placeholder="List Title">'
                 <div id="modal-buttons">
                     <button onclick="createList()">Create</button>
+                    <button class="destructive" onclick="abortModal()">Cancel</button>
+                </div>`;
+            case "new-task":
+                return `
+                <input id="new-task-name" type="text" placeholder="Task Name">'
+                <input id="new-task-desc" type="text" placeholder="Task Description">'
+                <input id="new-task-due-date" type="date" placeholder="Due: mm/dd/yyyy">'
+                <div id="modal-buttons">
+                    <button onclick="createTask('${id}')">Create</button>
                     <button class="destructive" onclick="abortModal()">Cancel</button>
                 </div>`;
         }
--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@
 // Application Start
 var localStorage = window.localStorage;
 
-refreshContent(State.login);
+if (User.current) {
+    refreshContent(State.myLists);
+} else {
+    refreshContent(State.login);
+}
 
 // Functions
-
-function hash(username, password) {
-    // Imported hashing algo
-    return hex_sha512(username + password)
-}
 
 function generateUUID() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -31,20 +30,19 @@ function login() {
 
     if (username && password) {
         User.current = new User(username, password)
-        document.getElementById("login-button").innerText = User.current?.username ?? "No Username";
         refreshContent(State.myLists)
     }
 }
 
-function toggleDropDown() {
+function toggleDropDown(condition) {
     const dropDown = document.getElementById("drop-down");
     const icon = document.getElementById("drop-down-icon");
-    const bool = dropDown.offsetHeight === 0;
+    const bool = condition ?? dropDown.offsetHeight === 0;
 
     dropDown.innerHTML = HTMLGenerator.generateDropDown(User.current?.lists ?? []);
 
     if (bool) {
-        dropDown.style.height = "90vw";
+        dropDown.style.height = "90vh";
         dropDown.style.overflow = "visible";
         icon.style.transform = "rotate(90deg)";
     } else {
@@ -54,10 +52,22 @@ function toggleDropDown() {
     }
 }
 
+function toggleItem(id, listId) {
+    let list = User.current.removeList(listId)
+
+    if (list) {
+        let task = list.removeTask(id);
+        list.push(new Task(task.title, task.dueDate, task.description, document.getElementById(id + "-checkbox").checked, id));
+
+        User.current.addList(list);
+    }
+}
+
 function refreshContent(state, listId) {
-    console.log(state);
     let html = "";
     let errorMessage = "";
+
+    document.getElementById("login-button").innerText = User.current?.username ?? "Login";
 
     switch (state) {
         case State.home:
@@ -67,8 +77,8 @@ function refreshContent(state, listId) {
         case State.listDetail:
             console.log("Generating single list");
             if (User.current) {
-                for (let list of User.current?.lists ?? []) {
-                    if (list.id === listId) {
+                for (let list of User.current.lists) {
+                    if (list.id == listId) {
                         html = HTMLGenerator.generateList(list);
                         break;
                     }
@@ -96,8 +106,8 @@ function refreshContent(state, listId) {
     }
 }
 
-function displayModal(state) {
-    document.getElementById("modal-content").innerHTML = HTMLGenerator.generateModal(state);
+function displayModal(state, id) {
+    document.getElementById("modal-content").innerHTML = HTMLGenerator.generateModal(state, id);
     document.getElementById("modal-popover").style.display = "flex";
 }
 
@@ -118,12 +128,57 @@ function createList() {
     }
 }
 
+function createTask(id) {
+    const name = document.getElementById("new-task-name").value;
+    const desc = document.getElementById("new-task-desc").value;
+    const dueDate = document.getElementById("new-task-due-date").value;
+    console.log("Creating new task:");
+    console.log("name: " + name);
+    console.log("desc: " + desc);
+    console.log("dueDate: " + dueDate);
+
+    console.log("Attempting to find list from id: " + id);
+    let list = User.current.removeList(id);
+    console.log(list ? "Found " + list : "No list found with matching id");
+    
+    if (list) {
+        list.tasks.push(new Task(name, dueDate, desc))
+        User.current.addList(list);
+        abortModal();
+        refreshContent(State.listDetail, id);
+    }
+}
+
+function clearCompleted(id) {
+    let list = User.current.removeList(id);
+    if (list) {
+        list.clearCompletedTodos();
+        User.current.addList(list);
+        refreshContent(State.listDetail, id);
+    }
+}
+
+function deleteTask(id, listId) {
+    let list = User.current.removeList(listId);
+
+    if (list) {
+        list.removeTask(id);
+        User.current.addList(list);
+        refreshContent(State.listDetail, listId);
+    }
+}
+
+function deleteList(id) {
+    User.current.removeList(id);
+    refreshContent(State.myLists);
+}
+
 function myListsState() {
     refreshContent(State.myLists);
 }
 
 function listState(id) {
-    refreshContent(State.listDetail);
+    refreshContent(State.listDetail, id);
 }
 
 function homeState() {
